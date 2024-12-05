cbb068c201dba7efde84a0e358cc34b3b758f622
Muhammad Maaz <mmaazjatt497@gmail.com>
2024-12-05 21:06:42 +0500

    i solved some error

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import logo from "../../../public/images/logo.jpg"
 import Image from 'next/image';
 const Hero = () => {
   return (
     <div className="container grid grid-cols-2 justify-center ml-[150px]">
-      <div className="w-52 h-52 rounded-full overflow-hidden flex">    
-        <Image src="/images/logo.jpg" alt="" className="w-full h-full object-cover"/>
+      <div  id='logo' className="w-52 h-52 rounded-full overflow-hidden flex">    
+        <Image src={logo} alt="logo" className="w-full h-full object-cover"/>
+        <img src="" alt="" />
       </div>
       <div className="text-[80px] font-medium">
         <p>I am</p>
